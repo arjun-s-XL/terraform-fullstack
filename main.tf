8c8fd0040c86fc744661496951416c586b86ea6a
arjun-s-XL <arjun.s@xoriant.com>
2024-09-12 06:20:21 +0000

    initial commit pre-deployment

---
+++ main.tf
@@ -0,0 +1,136 @@
+terraform {
+  backend "s3" {
+    bucket         = "your-s3-bucket-name"
+    key            = "terraform/statefile.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "your-dynamodb-table"  # Optional but recommended for state locking
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main_vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main_gateway"
+  }
+}
+
+resource "aws_instance" "flask" {
+  ami                  = var.ami_id
+  instance_type        = var.instance_type
+  subnet_id            = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.flask_sg.id]
+  tags = {
+    Name = "flask-instance"
+  }
+}
+
+resource "aws_instance" "nginx" {
+  ami                  = var.ami_id
+  instance_type        = var.instance_type
+  subnet_id            = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
+  tags = {
+    Name = "nginx-instance"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami                  = var.ami_id
+  instance_type        = var.instance_type
+  subnet_id            = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.mysql_sg.id]
+  tags = {
+    Name = "mysql-instance"
+  }
+}
+
+resource "aws_security_group" "flask_sg" {
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "flask-sg"
+  }
+}
+
+resource "aws_security_group" "nginx_sg" {
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "nginx-sg"
+  }
+}
+
+resource "aws_security_group" "mysql_sg" {
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  tags = {
+    Name = "mysql-sg"
+  }
+}
+
